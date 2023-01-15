007ff451b539c5cf9e8e62619f96d884dce4415c
Sergey Filimonov <srfilimonoff@gmail.com>
2023-01-15 22:05:18 +0300

    Creating DOM Elements

--- starter/script.js
+++ starter/script.js
@@ -61,150 +61,47 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+////////////////////////////////////////////////////////////
+// 147 Creating DOM Elements
 
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
+// показывать движение // получаем данные с которыми должно работать
+// параметр функции наз. движение
+const displayMovements = function (movements) {
+  // Свойство интерфейса Element innerHTML устанавливает или получает HTML или XML разметку дочерних элементов.
+  // опорожнить весь контейнер и только потом добовляем новые элементы
+  // Возвращает всё включая HTML, все тэги
+  // containerMovements.innerHTML = ``; // устанавливаем его в пустую строку
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+  // функция получает один массив движения, затем работает с этими данными
+  movements.forEach(function (mov, i) {
+    // функция (mov, i) обратного вызова
+    // Способ узнать дипозит или вывод средств при помощи троичного оператора
+    const type = mov > 0 ? `deposit` : `withdrawal`;
+    // создаю шаблонный литерал ``
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>`;
+    // insertAdjacentHTML() разбирает указанный текст как HTML или XML и вставляет полученные узлы (nodes) в DOM дерево в указанную позицию. Данная функция не переписывает имеющиеся элементы, что предотвращает дополнительную сериализацию и поэтому работает быстрее, чем манипуляции с innerHTML.
+    containerMovements.insertAdjacentHTML(`afterbegin`, html);
+  });
+  // создание html
+};
+displayMovements(account1.movements); // вызов функции
 
-/////////////////////////////////////////////////
-// 142 Simple Array Methods // Простые методы массива
+/////////////////////////////////////////////////////////////////
+// 151 Computing Usernames // Вычисление имен пользователей
 
-// let arr = [`a`, `b`, `c`, `d`, `e`];
-
-// // SLICE
-// // Метод slice() возвращает новый массив, содержащий копию части исходного массива.
-// console.log(arr.slice(2)); // (3) ['c', 'd', 'e']
-// console.log(arr.slice(2, 4)); // (2) ['c', 'd']
-// console.log(arr.slice(-2)); // (2) ['d', 'e']
-// console.log(arr.slice(-1)); //  ['e']
-// console.log(arr.slice(1, -2)); // (2) ['b', 'c']
-// console.log(arr.slice()); // (5) ['a', 'b', 'c', 'd', 'e']
-// console.log([...arr]); // (5) ['a', 'b', 'c', 'd', 'e']
-
-// // SPLICE
-// // Метод splice() изменяет содержимое массива, удаляя существующие элементы и/или добавляя новые.
-
-// // console.log(arr.splice(2)); // (3) ['c', 'd', 'e']
-// // console.log(arr); //  ['a', 'b'] // измененный массив
-
-// // arr.splice(-1); // удалил `e`
-// // console.log(arr); // (4) ['a', 'b', 'c', 'd']
-// arr.splice(-1);
-// console.log(arr); // ['a', 'b', 'c', 'd']
-// arr.splice(1, 2);
-// console.log(arr); // ['a', 'd']
-
-// // REVERSE
-// // Метод reverse() на месте обращает порядок следования элементов массива. Первый элемент массива становится последним, а последний — первым.
-
-// arr = [`a`, `b`, `c`, `d`, `e`];
-// const arr2 = [`j`, `i`, `h`, `g`, `j`];
-// console.log(arr2.reverse()); // (5) ['j', 'g', 'h', 'i', 'j']
-// console.log(arr2); // (5) ['j', 'g', 'h', 'i', 'j']  // мутирует исходный массив
-
-// // CONCAT
-// // Метод concat() возвращает новый массив, состоящий из массива, на котором он был вызван, соединённого с другими массивами и/или значениями, переданными в качестве аргументов.
-
-// const letters = arr.concat(arr2);
-// console.log(letters); // (10) ['a', 'b', 'c', 'd', 'e', 'j', 'g', 'h', 'i', 'j']
-// console.log([...arr, ...arr2]); // не мутирует массивы, они остаются в первоночальной версии
-
-// JOIN
-
-// Метод join() объединяет все элементы массива (или массивоподобного объекта) в строку.
-// Определяет строку, разделяющую элементы массива. В случае необходимости тип разделителя приводится к типу Строка. Если он не задан, элементы массива разделяются запятой ','. Если разделитель - пустая строка, элементы массива ничем не разделяются в возвращаемой строке
-
-// console.log(letters.join(` - `)); // строка // a - b - c - d - e - j - g - h - i - j
-
-////////////////////////////////////////////////////
-// 143 The new at Method // Новый метод at
-
-// Метод at() принимает значение в виде целого числа и возвращает элемент массива с данным индексом. В качестве аргумента метод принимает положительные и отрицательные числа. При отрицательном значении отсчёт происходит с конца массива.
-
-// const arr = [23, 11, 64];
-// console.log(arr[0]); // 23
-// console.log(arr.at(0)); // 23 // точка массива в нулевой позиции
-
-// // getting last array element // получение последнего элемента массива
-// console.log(arr[arr.length - 1]); // 64
-// console.log(arr.slice(-1)[0]); // 64 // что бы убрать скобки стаивтся [0] // получаем ценность массива
-// console.log(arr.at(-1)); // 64
-
-// console.log(`Sergey`.at(0)); // S
-// console.log(`Sergey`.at(-1)); // y
-
-////////////////////////////////////////////////////
-// 144 Looping Arrays_ forEach.en_US // Циклические массивы_  forEach.en_US
-
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-// // Object.entries() метод возвращает массив собственных перечисляемых свойств указанного объекта в формате [key, value], в том же порядке, что и в цикле for...in
-
-// for (const [i, movement] of movements.entries()) {
-//   if (movement > 0) {
-//     console.log(`Movement ${i + 1}: You withdrew deposited ${movement}`);
-//   } else {
-//     // Метод Math.abs() возвращает абсолютное значение числа. то есть
-//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
-//   }
-// }
-
-// console.log(`--------------- FOREACH -----------------`);
-// // не работают в FOREACH - CONTINUE, BREAK
-// // Метод forEach() выполняет указанную функцию один раз для каждого элемента в массиве.
-// // функция обратного вызова  // forEach евляется функцией высокого порядка
-// // Имеет значене порядок аргументов, первый параметр - текущий элемент, второй - текущий индекс, третий весь массив
-// // movement, index, array
-// movements.forEach(function (mov, i, arr) {
-//   if (mov > 0) {
-//     console.log(`Movement ${i + 1}: You withdrew deposited ${mov}`);
-//   } else {
-//     // Метод Math.abs() возвращает абсолютное значение числа. то есть
-//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
-//   }
-// });
-
-// 0: function(200)
-// 1: function(450)
-// 2: function(400)
-
-////////////////////////////////////////////////////////////////
-// 145 forEach With Maps and Sets.en_US
-
-// Map
-// Метод map() создаёт новый массив с результатом вызова указанной функции для каждого элемента массива.
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
-
-// currencies.forEach(function (value, key, map) {
-//   console.log(`${key}: ${value}`);
-//   // USD: United States dollar
-//   // EUR: Euro
-//   // GBP: Pound sterling
-// });
-
-// // Set
-// const currenciesUnique = new Set([`USD`, `GBP`, `USD`, `EUR`, `EUR`]);
-// console.log(currenciesUnique); // Set(3) {'USD', 'GBP', 'EUR'}
-// // value, key, map имеется ввиду value, value, map, но так писать нельзя
-// currenciesUnique.forEach(function (value, _, map) {
-//   console.log(`${value}: ${value}`);
-//   // USD: USD
-//   // GBP: GBP
-//   // EUR: EUR
-// });
-
-////////////////////////////////////////////////////
-// 146 PROJECT_ _Bankist_ App
-
-console.log(5 + 1 + 17 + 6 + 13 + 6 + 10); // 14.01.22
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase() // steven thomas williams
+      .split(` `) // (3) ['steven', 'thomas', 'williams']
+      .map(name => name[0]) // ['s', 't', 'w']
+      .join(``); // stw
+  }); // здесь не нужен return, зпдача вызвать побочный эффект, мы не создаем новое значение для возврата
+};
+createUsernames(accounts);
