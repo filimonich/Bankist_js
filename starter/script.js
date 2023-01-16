da9755365221d21cdb9eb02189243d774a7d80a6
Sergey Filimonov <srfilimonoff@gmail.com>
2023-01-16 19:25:00 +0300

    The Magic of Chaining Methods

--- starter/script.js
+++ starter/script.js
@@ -101,6 +101,32 @@ const calcDisplayBalance = function (movements) {
 };
 calcDisplayBalance(account1.movements);
 
+///////////////////////////////////////////////
+//  155 The Magic of Chaining Methods // Магия методов объединения в цепочки
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`; // Метод Math.abs() возвращает абсолютное значение числа, убрал минус в начале числа
+
+  //сумма начисленных процентов, за пополнение счета
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      return int >= 1; // процент выплачивают, при условии что сумма процентов больше 1 в любой валюте
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
+
 /////////////////////////////////////////////////////////////////
 // 151 Computing Usernames // Вычисление имен пользователей
 
